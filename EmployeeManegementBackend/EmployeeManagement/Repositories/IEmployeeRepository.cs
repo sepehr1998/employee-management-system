d4ccad8f2b919e76477ec3ec628d4ff4adb7c6b5
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-03 19:06:25 +0200

    employee repository interface developed

---
+++ EmployeeManegementBackend/EmployeeManagement/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Repositories;
+
+public interface IEmployeeRepository
+{
+   Task<IEnumerable<Employee>> GetAllAsync();
+   Task<Employee> GetByIdAsync(int id);
+   Task AddEmployeeAsync(Employee employee);
+   Task UpdateEmployeeAsync(Employee employee);
+   Task DeleteEmployeeAsync(int id);
+}
