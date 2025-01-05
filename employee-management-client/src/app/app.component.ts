465c2bdb0cff4fa42e3b42fda24500aa5b0b5266
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-05 23:18:36 +0200

    employee model created. employee table component initiated

--- employee-management-client/src/app/app.component.ts
+++ employee-management-client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeTableComponent } from "./employee-table/employee-table.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
